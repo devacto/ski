1b37acdf4b81a493b4061529f15c8908950fadd6
Victor Wibisono <victor@eatigo.com>
2017-08-11 16:14:49 +0800

    Added small test files.

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 import sys
 
 def main():
-  print('Hello World!')
+    if len(sys.argv) != 2:
+        sys.exit('Usage: {} <map file>'.format(sys.argv[0]))
+
+    print('Hello World!')
 
 if __name__ == '__main__':
     main()
