7c0922f97597fe5af1a0e96dec29c1b0228a51e2
Victor Wibisono <victor@eatigo.com>
2017-08-13 22:40:46 +0800

    Added parse function.

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python
 
-import sys
+import sys, re
+
+def parse_map(path):
+    '''Creates a 2D array from the data, given the path.'''
+    with open(path) as file:
+        width, height = map(int, file.readline().split())
+        grid = []
+        for line in file:
+            line = re.sub('[\n]', '', line)
+            array = line.split(' ')
+            grid.append(array)
+    return width, height, grid
 
 def main():
     if len(sys.argv) != 2:
         sys.exit('Usage: {} <map file>'.format(sys.argv[0]))
 
-    print('Hello World!')
+    # Parsing
+    print('Parsing map data ...')
+    w, h, g = parse_map(sys.argv[1])
+    print('width = {}'.format(w))
+    print('height = {}'.format(h))
+
+    # Traversing
+    for y in range(0, w):
+        for x in range(0, h):
+            list = []
+            list.append(g[y][x])
+            print('at x:{}, y:{} with value {}'.format(x, y, g[y][x]))
+            print('up: {}; left: {}; right: {}, down: {}', )
+
 
 if __name__ == '__main__':
     main()
